f095bbcfabde78176c78a12b341923a11c5e6a1d
Jan <j-p_rogge@live.de>
2021-02-06 16:32:30 +0100

    Add option to link a voice channel to a role (#5)
    * Add trailer argument and guild_id to movie model
    
    * Add guild constrains
    
    * Add empty check
    
    * Cleanup movies and genre for servers that leave
    
    * Cleanup event handling
    
    * Add sequelize cli
    
    * Add voicerole linking

--- dbObjects.js
+++ dbObjects.js
@@ -20,8 +20,9 @@ const Movie = require('./models/Movies')(sequelize, Sequelize.DataTypes);
 const Genre = require('./models/Genre')(sequelize, Sequelize.DataTypes);
 const User_bans = require('./models/User_bans')(sequelize, Sequelize.DataTypes);
 const Settings = require('./models/Settings')(sequelize, Sequelize.DataTypes);
+const VoiceRoleLink = require('./models/VoiceRoleLink')(sequelize, Sequelize.DataTypes);
 
 Movie.belongsTo(Genre, { foreignKey: 'genre_id', as: 'genre' });
 
 
-module.exports = { Movie, Genre, User_bans, Settings };
+module.exports = { Movie, Genre, User_bans, Settings, VoiceRoleLink};
