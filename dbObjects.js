db8e70374d2cccfb711860427707b29d67506dd2
Jan Rogge <j-p_rogge@live.de>
2022-04-28 19:42:06 +0200

    Fix sql error

--- dbObjects.js
+++ dbObjects.js
@@ -3,11 +3,11 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	logging: false,
-	//dialectOptions: {
-	//	ssl: {
-	//		rejectUnauthorized: false,
-	//	},
-	//},
+	dialectOptions: {
+		ssl: {
+			rejectUnauthorized: false,
+		},
+	},
 });
 
 const Settings = require('./models/Settings')(sequelize, Sequelize.DataTypes);
